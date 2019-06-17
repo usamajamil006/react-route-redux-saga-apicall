04723a71a5813999ee66df6876f9adb2378e8bbb
usama-jamil <usamajamil006@gmail.com>
2019-06-18 02:17:10 +0500

    create  a  saga

---
+++ src/store/sagas/index.js
@@ -0,0 +1,6 @@
+function* rootSaga() {
+  console.log("heloo mamo");
+}
+
+export default rootSaga;
+ 
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,16 @@
 import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from './sagas'
 import rootReducer from "./reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware()
+
+
+const store = createStore(rootReducer, composeWithDevTools(
+    applyMiddleware(sagaMiddleware)
+));
+
+sagaMiddleware.run(rootSaga)
 
 export default store;
