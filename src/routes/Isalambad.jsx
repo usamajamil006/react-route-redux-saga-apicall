0b7e6d263f115abb293c7441860ceda3c7c16e8b
usama-jamil <usamajamil006@gmail.com>
2019-06-17 23:14:23 +0500

    Basic Components

---
+++ src/routes/Isalambad.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Isalambad extends Component {
+    render() {
+        return (
+            <div>
+                Islamabad
+            </div>
+        )
+    }
+}
---
+++ src/routes/Lahore.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Lahore extends Component {
+    render() {
+        return (
+            <div>
+                Lahore
+            </div>
+        )
+    }
+}
