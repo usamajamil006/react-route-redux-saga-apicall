4bff27de80e27748d991a775d26a0eb5a57ea1c0
usama-jamil <usamajamil006@gmail.com>
2019-06-17 03:41:02 -0700

    Creating routes for the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Karachi from './routes/Karachi'
+import Lahore from './routes/Lahore'
+import Islamabad from './routes/Isalambad'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className="app">
+      <Route path="/" component={Karachi} exact />
+      <Route path="/search" component={Lahore} />
+      <Route path="/search" component={Islamabad} />
     </div>
+  </BrowserRouter>
   );
 }
 
