0418b7a366773c91b7195e4cf679637ab8cca3a1
usama-jamil <usamajamil006@gmail.com>
2019-06-19 14:46:56 +0500

    Class to function component

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,13 +1,12 @@
-import React, { Component } from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 
-export default class NavBar extends Component {
-    render() {
-        return (<div>
-            <Link to='/'>Karachi</Link>
-            <Link to='/lahore'>Lahore</Link>
-            <Link to='/islamabad'>Islamabad</Link>
-            </div>
-            )
-    }
-}
+const NavBar = () => (
+  <div>
+    <Link to="/">Karachi</Link>
+    <Link to="/lahore">Lahore</Link>
+    <Link to="/islamabad">Islamabad</Link>
+  </div>
+);
+
+export default NavBar;
