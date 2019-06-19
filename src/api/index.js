b921f00fb5ed84a3825c441abe378f688be881b2
usama-jamil <usamajamil006@gmail.com>
2019-06-19 14:47:32 +0500

    Location api Weather api

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,33 @@
 const KEY = "mWW36XM9XBprnpfIZCKovK5GKL3HoB2i";
 
+// const fetchWeather = async city => {
+//   const res = await fetch(
+//     `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${KEY}&q=${city}&offset=2`
+//   );
+//   const data = await res.json();
+//   console.log(data[0])
+//   if (!data) {
+//     throw new Error(data.errors);
+//   }
+
+//   return data[0];
+// };
 const fetchWeather = async city => {
   const res = await fetch(
-    `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${KEY}&q=${city}&offset=2`
+    `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${city}?apikey=${KEY}&metric=true`
   );
   const data = await res.json();
-  console.log(data[0])
+  console.log(data)
   if (!data) {
     throw new Error(data.errors);
   }
 
-  return data[0];
+  return {
+    Headline: data.Headline.Text,
+    MinTemperature: data.DailyForecasts[0].Temperature.Minimum.Value,
+    MaxTemperature: data.DailyForecasts[0].Temperature.Maximum.Value,
+    Day: data.DailyForecasts[0].Day.IconPhrase,
+    Night: data.DailyForecasts[0].Night.IconPhrase
+  };
 };
 export {fetchWeather};
